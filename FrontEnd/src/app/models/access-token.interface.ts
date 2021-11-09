b7ac115dc37a39bed50448f23e29f907a8365159
adesafta2002 <ade.safta@yahoo.com>
2021-11-09 15:43:28 +0200

    fixed

--- FrontEnd/src/app/models/access-token.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface IAccessToken {
-    accessToken: string;
-    expiresIn: number;
-}
--- FrontEnd/src/app/modules/auth/container/auth-main.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'auth-main',
-  templateUrl: 'auth-main.component.html',
-  styleUrls: ['auth-main.component.scss']
-})
-export class AuthComponent {
-}
